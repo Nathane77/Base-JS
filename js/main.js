cd1f6c1ff5a6ba95bb04a5c2b14ae20e644be307
Nathane77 <hamidoulila14@gmail.com>
2024-11-28 17:00:56 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+alert("test");
+
+let color = document.getElementsByClassName("square").style.color;
+let height = document.getElementsByClassName("square").style.height;
+let width = document.getElementsByClassName("square").style.width;
+let display = document.getElementsByClassName("square").style.display;
+let font = document.getElementsByClassName("square").style.font-family;
+
