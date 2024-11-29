10a12eee75492eca695eb41ec165513cdbc1480c
Nathane77 <hamidoulila14@gmail.com>
2024-11-29 09:29:50 +0100

    finished the project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-alert("test");
 
-let color = document.getElementsByClassName("square").style.color;
-let height = document.getElementsByClassName("square").style.height;
-let width = document.getElementsByClassName("square").style.width;
-let display = document.getElementsByClassName("square").style.display;
-let font = document.getElementsByClassName("square").style.font-family;
 
+const id = window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("class");
+const color = window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("background-color");
+const height = window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("height");
+const width =  window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("width");
+const display =  window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("display");
+const font =  window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("font-family");
+
+
+
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener("click", showInfo);
+
+function showInfo(){
+    alert("information du carré  \n\tcouleur: "+ color+
+   "\n\thauter: "+ height+
+   "\n\tlargeur: "+ width+
+   "\n\tafichage: "+ display+
+   "\n\tfont:"+ font
+);
+}
