49e8bf2421fad0d48e4f9c25a45a65c52ae8f6ad
Nathane77 <hamidoulila14@gmail.com>
2024-11-29 09:38:22 +0100

    commented the code

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
+//assigns the span with the id btn to the const btn
+const btn = document.querySelector('#btn');
 
-
-const id = window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("class");
+// assigns each css value to the coresponding const
+const id = document.querySelector('.square').className;
 const color = window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("background-color");
 const height = window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("height");
 const width =  window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("width");
 const display =  window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("display");
 const font =  window.getComputedStyle(document.querySelector('.square'), null).getPropertyValue("font-family");
 
-
-
-
-const btn = document.querySelector('#btn');
-
+//add the checker for the click on the span
 btn.addEventListener("click", showInfo);
 
+//function to show the alert with all the const of information
 function showInfo(){
-    alert("information du carré  \n\tcouleur: "+ color+
+    alert("information de "+id+" \n\tcouleur: "+ color+
    "\n\thauter: "+ height+
    "\n\tlargeur: "+ width+
    "\n\tafichage: "+ display+
-   "\n\tfont:"+ font
+   "\n\tfont:"+ font // \n = new line \t = indentation
 );
 }
