0c22de31ecc7ac54dda0b88b4dfb16e00f733702
Nathane77 <hamidoulila14@gmail.com>
2024-12-02 17:02:31 +0100

    made the css and html structure of the exo

---
+++ exo5/js/main.js
@@ -0,0 +1,10 @@
+// import * from "./quotes.js";
+
+const quoteDiv = document.querySelectorAll('.quote'); 
+const name = document.querySelectorAll('.name'); 
+
+// quotes.forEach(quote => {
+//     quoteDiv.textContent = quote;
+// });
+
+// console.log(quotes);
